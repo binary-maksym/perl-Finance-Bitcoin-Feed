e465d72bd1b4013d60106b636588cde61beb4b8b
Chylli <chylli.email@gmail.com>
2014-12-05 15:17:53 +0800

    coinsetter pod ok

--- lib/Finance/Bitcoin/Feed/Site/CoinSetter.pm
+++ lib/Finance/Bitcoin/Feed/Site/CoinSetter.pm
@@ -8,6 +8,10 @@ has ws_url => 'https://plug.coinsetter.com:3000/socket.io/1';
 has 'ua';
 has 'site' => 'CoinSetter';
 
+# this site need 2 handshakes
+# 1. get session id by http GET method 
+# 2. generate a new url by adding session id to the old url
+# 3. connect by socket id
 sub go {
     my $self = shift;
     $self->SUPER::go;
@@ -136,7 +140,7 @@ sub parse {
         $packet->{data} = $data || '';
     }
 
-#"5:::{"name":"last","args":[{"price":367,"size":0.03,"exchangeId":"COINSETTER","timeStamp":1417382915802,"tickId":14667678802537,"volume":14.86,"volume24":102.43}]}"
+		#"5:::{"name":"last","args":[{"price":367,"size":0.03,"exchangeId":"COINSETTER","timeStamp":1417382915802,"tickId":14667678802537,"volume":14.86,"volume24":102.43}]}"
     elsif ( $packet->{type} eq 'event' ) {
         eval {
             my $opts = decode_json($data);
@@ -167,130 +171,38 @@ sub parse {
         $tx->send( { text => qq(2:::) } );
     }
     elsif ( $packet->{type} eq 'disconnect' ) {
-        $tx->owner->set_timeout();
+			$tx->owner->debug('disconnected by server');
+			$tx->owner->set_timeout();
     }
 }
 
 1;
+
 __END__
 
 =head1 NAME
 
-Finance::Bitcoin::Feed::Site - Base class of Finance::Bitcoin::Feed modules
-
-
-=head1 VERSION
-
-This document describes BitCoinFeed version 0.0.1
+Finance::Bitcoin::Feed::Site::CoinSetter -- the class that connect and process the data from site Coinsetter
 
 
 =head1 SYNOPSIS
 
-    use Mojo::Base 'Finance::Bitcoin::Feed::Site';
+    use Mojo::Base 'Finance::Bitcoin::Feed::Site::CoinSetter';
 
-    sub go{
-       my $self = shift;
-       .....
-    }
+    my $obj = Finance::Bitcoin::Feed::Site::BitStamp->new();
+    $obj->go();
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
+    # dont forget this 
+    AnyEvent->condvar->recv;
   
 =head1 DESCRIPTION
 
-   It is a base class
+Connect to site BitStamp by protocol socket.io v 0.9.6
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
+=head1 SEE ALSO
 
-
-=head1 INTERFACE 
-
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
-
-
-=head1 DIAGNOSTICS
-
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-BitCoinFeed requires no configuration files or environment variables.
-
-
-=head1 DEPENDENCIES
-
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
-
-=head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
-None reported.
-
-
-=head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<bug-bitcoinfeed@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
-
+L<Finance::Bitcoin::Feed::Site>, L<https://www.coinsetter.com/api>,
+L<https://github.com/Automattic/socket.io-parser>
 
 =head1 AUTHOR
 
