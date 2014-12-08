40eb50291da72ddcb8b83f352f1534a3284eda02
Chylli <chylli.email@gmail.com>
2014-12-08 08:53:47 +0800

    remove license line in feed.pm

--- lib/Finance/Bitcoin/Feed.pm
+++ lib/Finance/Bitcoin/Feed.pm
@@ -117,8 +117,3 @@ Chylli  C<< <chylli@binary.com> >>
 =head1 COPYRIGHT
 
 Copyright 2014- Binary.com
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
